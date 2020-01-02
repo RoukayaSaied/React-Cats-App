c899d725528be6316bcdafd8998e52d198ef31b0
rsaied <roukaya.saied@talan.com>
2020-01-02 17:16:25 +0100

    Updates

---
+++ precache-manifest.1607490be7c7428a9f532fd80366dc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10ac1de002696c7a86aa5f900fb9787",
+    "url": "/React-Cats-App/index.html"
+  },
+  {
+    "revision": "ed1899ee6e4d9e07439f",
+    "url": "/React-Cats-App/static/css/main.57773c2b.chunk.css"
+  },
+  {
+    "revision": "e0d39c18bba7e44ec9e4",
+    "url": "/React-Cats-App/static/js/2.b01b327e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Cats-App/static/js/2.b01b327e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed1899ee6e4d9e07439f",
+    "url": "/React-Cats-App/static/js/main.f4cf8ccb.chunk.js"
+  },
+  {
+    "revision": "9e98d818495ef768c978",
+    "url": "/React-Cats-App/static/js/runtime-main.612377ca.js"
+  }
+]);
